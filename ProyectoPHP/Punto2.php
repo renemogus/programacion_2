d9ce0a9297a4fe08770c1b4467461cf1c95b0d3f
renemogus <127851079+renemogus@users.noreply.github.com>
2023-05-16 11:28:10 -0300

    Update Punto2.php

--- ProyectoPHP/Punto2.php
+++ ProyectoPHP/Punto2.php
@@ -1,27 +1,7 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Comparar valores, pero más complejo.</title></head>
-<body>
-	<form method="post">
-		<label for="valor1">Introduzca el valor 1:</label>
-		<input type="number" name="valor1" id="valor1"><br><br>
-		<label for="valor2">Introduzca el valor 2:</label>
-		<input type="number" name="valor2" id="valor2"><br><br>
-		<input type="submit" value="Comparar">
-
-	</form> 
-	<?php
-	if (isset($_POST['valor1']) && isset($_POST['valor2'])) {
-		$valor1 = $_POST['valor1'];
-		$valor2 = $_POST['valor2']; 
-		if ($valor1 < $valor2) {
-	echo "El valor dos es mayor que el uno.";
-		} else if ($valor1 > $valor2) {
-			echo "El valor uno es mayor que el dos.";
-		} else if ($valor1 == $valor2) {
-			echo "El valor uno y dos son iguales.";
-		}
-	<?>
-</body>
-</html>
+<?php
+$valor1=20
+$valor2=30
+if ($valor1<$valor2) {
+    echo "El valor dos es mayor que el uno"
+}
+?>
